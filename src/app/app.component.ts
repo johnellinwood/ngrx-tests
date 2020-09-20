d0bee1743e4932a4644f95e425e0a3a353731d41
John Ellinwood <john.ellinwood@nissint.com>
2020-09-19 22:01:44 -0400

    First commmit. Working example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {selectItems, State} from './app.reducers';
+import {Observable} from 'rxjs';
+import * as ItemActions from './app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'store-test';
+
+  item$: Observable<string[]>;
+
+  constructor(private store: Store<State>) { }
+
+  ngOnInit(): void {
+    this.item$ = this.store.pipe(select(selectItems));
+  }
+
+  loadItems(): void {
+    console.log('Load button pressed');
+    this.store.dispatch(new ItemActions.Load());
+  }
+
+  clearItems() {
+    console.log('Clear button pressed');
+    this.store.dispatch(new ItemActions.Clear());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {reducers} from './app.reducers';
+import {EffectsModule} from '@ngrx/effects';
+import {ItemEffects} from './app.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([ItemEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
