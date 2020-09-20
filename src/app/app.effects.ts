7cdd4b4660235a7cff4aa4661e114a6cfa8bb863
John Ellinwood <john.ellinwood@nissint.com>
2020-09-20 09:40:35 -0400

    making enterprisey

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,17 +1,22 @@
 import {Injectable} from '@angular/core';
 import {defer, of} from 'rxjs';
-import {Store} from '@ngrx/store';
-import {Actions, Effect} from '@ngrx/effects';
+import {Actions, createEffect, Effect} from '@ngrx/effects';
 import {State} from './app.reducers';
 import * as ItemActions from './app.actions';
+import {tap} from 'rxjs/operators';
 
 const initialState: string[] = ['one', 'two', 'three'];
 
 @Injectable()
 export class ItemEffects {
 
-  constructor(private actions$: Actions, private store: Store<State>) {
+  constructor(private actions$: Actions) {
   }
+
+  logActions$ = createEffect(() =>
+    this.actions$.pipe(
+      tap(action => console.log('log action', action))
+    ), { dispatch: false });
 
   @Effect()
   init$ = defer(() => of(new ItemActions.Load(initialState)));
